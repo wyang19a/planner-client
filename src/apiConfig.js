0b5b35fcc2e80744af7985506b6ae9aff6494c9c
wyang19a <wyang19a@gmail.com>
2020-01-24 16:19:19 -0500

    Added production apiConfig url
    production: https://sheltered-waters-25858.herokuapp.com/
    development: http://localhost:4741

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://sheltered-waters-25858.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
