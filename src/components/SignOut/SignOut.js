9ec3c2971978f1da2890d6076f7a62179c15a85c
wyang19a <wyang19a@gmail.com>
2020-01-24 19:06:24 -0500

    Fix header component home, SignOut component message, added title

--- src/components/SignOut/SignOut.js
+++ src/components/SignOut/SignOut.js
@@ -11,7 +11,7 @@ class SignOut extends Component {
     signOut(user)
       .finally(() => alert({
         heading: 'Signed Out Successfully',
-        messagE: messages.signOutSuccess,
+        message: messages.signOutSuccess,
         variant: 'success'
       }))
       .finally(() => history.push('/'))
