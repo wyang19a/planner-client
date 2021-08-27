46d1acbc1a2dbd9603e631de487eb31e1ea9b966
wyang19a <wyang19a@gmail.com>
2021-08-27 16:10:46 -0400

    Header change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,53 @@
-import React, { Fragment } from 'react'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTasks } from '@fortawesome/free-solid-svg-icons'
+import React, { Fragment } from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTasks } from "@fortawesome/free-solid-svg-icons";
 
 const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
   </Fragment>
-)
+);
 
 const unauthenticatedOptions = (
   <Fragment>
     <Nav.Link href="#sign-up">Sign Up</Nav.Link>
     <Nav.Link href="#sign-in">Sign In</Nav.Link>
+    <Nav.Link href="#sign-in">Change Password</Nav.Link>
   </Fragment>
-)
+);
 
 const alwaysOptions = (
   <Fragment>
     <Nav.Link href="#/tasks">Home</Nav.Link>
   </Fragment>
-)
+);
 
 const Header = ({ user }) => (
   <Navbar className="navbar" variant="dark" expand="md">
     <Navbar.Brand href="#/tasks">
       <div className="app-name-container">
-        <FontAwesomeIcon className="icon" style={{ marginRight: '15px' }} icon={faTasks} />
+        <FontAwesomeIcon
+          className="icon"
+          style={{ marginRight: "15px" }}
+          icon={faTasks}
+        />
         <div className="app-name">Get It Done !</div>
       </div>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
-        { alwaysOptions }
-        { user ? authenticatedOptions : unauthenticatedOptions }
+        {user && (
+          <span className="navbar-text mr-2">Welcome, {user.email}</span>
+        )}
+        {alwaysOptions}
+        {user ? authenticatedOptions : unauthenticatedOptions}
       </Nav>
     </Navbar.Collapse>
   </Navbar>
-)
+);
 
-export default Header
+export default Header;
